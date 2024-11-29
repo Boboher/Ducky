3f78de73ff18dac2af75b9e50fbabc0fcde37637
Boboher <92533902+Boboher@users.noreply.github.com>
2024-11-29 16:58:00 +0100

    Update Exam_setup.ps1

--- Exam_setup.ps1
+++ Exam_setup.ps1
@@ -1,3 +1,5 @@
 $spotifyAppId = (Get-StartApps | Where-Object {$_.Name -like "*Spotify*"}).AppId
 Start-Process $spotifyAppId
 Start-Process MSEdge https://kavoskenslaan.smartschool.be
+Start-Sleep -Seconds 3
+Start-Process MSEdge https://open.spotify.com/track/1LVKWm2sixcBd1sqqrvNNU?si=d17105e918524c3c
